b9ff660b0da85fb9d658c26cc06f6dc3faf52227
Ashok Kumar Pandey <Ashok@RELYON.COM>
2018-07-02 14:36:38 +0530

    modified version

--- angular-demo/src/app/employee/employeeCount.component.ts
+++ angular-demo/src/app/employee/employeeCount.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'employee-count',
-    templateUrl: 'app/employee/employeeCount.component.html',
-    styleUrls: ['app/employee/employeeCount.component.css']
+    templateUrl: '../employee/employeeCount.component.html',
+    styleUrls: ['../employee/employeeCount.component.css']
 })
 export class EmployeeCountComponent {
     all: number = 10;
--- angular-demo/src/app/employee/employeeList.component.ts
+++ angular-demo/src/app/employee/employeeList.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-
 @Component({
     selector: 'list-employee',
-    templateUrl: 'app/employee/employeeList.component.html',
-    styleUrls: ['app/employee/employeeList.component.css']
+    templateUrl: '../employee/employeeList.component.html',
+    styleUrls: ['../employee/employeeList.component.css']
 })
 export class EmployeeListComponent {
     employees: any[];
